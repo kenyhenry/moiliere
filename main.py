dd854d1d1de59205b04e8ca9cd098aa1c731903f
keet <henry.keny@outlook.fr>
2025-07-22 00:18:08 -0400

    mv

--- main.py
+++
@@ -1,7 +0,0 @@
-from moliere.cli import app
-import typer
-
-if __name__ == "__main__":
-    app()
-    # path = "/Users/keet/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/projets/caribbeancode/template"
-    # path = "/Users/keet/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/projets/caribbeancode/template/caribbeancode.html"
